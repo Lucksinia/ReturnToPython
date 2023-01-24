c4fd4b4c3484eef00b051f6a5ff23fa765ae06ab
Lucksinia <denni334334@gmail.com>
2023-01-24 23:27:29 +0300

    some proggress

--- bdayparadox.py
+++ bdayparadox.py
@@ -32,6 +32,45 @@ def main():
         """Birthday Paradox, by Lucksinia
         In a group of N people, odds of some of them having birthdays on the
         same day is suprisingly large
-        To express that, we can generate 100000 simulations   
+        To express that, we can generate 100 000 simulations from wich we
+        then extrapolate probabylity for such occurence. 
     """
     )
+
+    Months = {
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    }
+
+    while True:
+        print("How many birthdays shoud we generate?")
+        responce = input("~> ")
+        if responce.isdecimal() and (0 < int(responce) <= 100):
+            numbdays = int(responce)
+            break
+    print()
+    print(f"There have been {numbdays} Birthdays generated:")
+    birthdays = get_birthdates(numbdays)
+    for i, birthday in enumerate(birthdays):
+        if i != 0:
+            print(", ", end="")
+            monthname = Months[birthday.month - 1]
+            dateText = f"{monthname} {birthday.day}"
+            print(dateText, end="")
+    print("\n")
+    match = get_matched(birthdays)
+
+    print("In this simulation, ", end="")
+    if match != None:
+        monthname = Months[match.month - 1]
