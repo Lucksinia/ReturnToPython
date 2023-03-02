218d47e69130fd6bd46a55fe3583a5ee99170cb6
Lucksinia <denni334334@gmail.com>
2023-03-03 00:02:06 +0300

    added first letter-by-letter implementation.

--- beersong.py
+++ beersong.py
@@ -3,21 +3,26 @@ Fist "Program" to remember HOW TO CODE.
 Generate song about beer with right bottle count and word forms.
  From "Head First Python 2 edition" by Paul Berry
 """
-import threading
 
-# implement letter-by-letter some day in the future.
-def ochprint(str, ms=1):
-    pass
+import sys
+from time import sleep
+
+# TODO: Re-implement letter-by-letter some day in the future.
+def ochprint(str, ms=0.02):
+    for ch in str:
+        sys.stdout.write(ch)
+        sys.stdout.flush()
+        sleep(ms)
 
 
 word = "bottles"
 for bottles_num in range(99, 0, -1):
-    print(f"{bottles_num} {word} of beer left on the wall!")
-    print(f"{bottles_num} {word} of beer.")
-    print("Take one down.")
-    print("Pass it around.")
+    ochprint(f"{bottles_num} {word} of beer left on the wall!\n")
+    ochprint(f"{bottles_num} {word} of beer.\n")
+    ochprint("Take one down.\n")
+    ochprint("Pass it around.\n")
     print("---------------")
     if bottles_num == 1:
-        print("No more bottles left on the wall...")
+        ochprint("No more bottles left on the wall...\n")
     elif bottles_num == 2:
-        print(f"{bottles_num} bottle of beer on the wall!")
+        ochprint(f"{bottles_num} bottle of beer on the wall!\n")
