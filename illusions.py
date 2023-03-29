a1c7410618434b4be05722ceca1ed76e340ebc6a
Lucksinia <denni334334@gmail.com>
2023-03-29 12:45:41 +0300

    updated illusions to be animated.

--- illusions.py
+++ illusions.py
@@ -6,17 +6,17 @@ turtle.bgcolor("white")
 
 
 def circles():
+    turtle.hideturtle()
     turtle.speed(0)
-    turtle.tracer(0)
     turtle.penup()
     turtle.goto(0, 0)
     turtle.pendown()
     turtle.setheading(0)
 
-    colours = ["#FF0000", "#00FF00", "#0000FF"]
+    colours = ["#FF0000", "#00FF00", "#0000FF", "#0F00FF", "#F0FF0F"]
 
     for i in range(361):
-        turtle.color(colours[i % 3])
+        turtle.color(colours[i % 5])
         turtle.circle(100)
         turtle.left(1)
 
